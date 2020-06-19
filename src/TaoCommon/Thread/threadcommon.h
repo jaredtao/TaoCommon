6a3c32661245032240e7e634b7dec0e3620591ea
jared <jared2020@163.com>
2020-06-19 20:56:39 +0800

    update for warning

--- src/TaoCommon/Thread/threadcommon.h
+++ src/TaoCommon/Thread/threadcommon.h
@@ -5,6 +5,6 @@ namespace TaoCommon
 {
     using WorkCallback = std::function<bool()>;
     using WorkResultCallback = std::function<void(bool)>;
-    Q_DECLARE_METATYPE(WorkCallback);
-    Q_DECLARE_METATYPE(WorkResultCallback);
 }
+Q_DECLARE_METATYPE(TaoCommon::WorkCallback);
+Q_DECLARE_METATYPE(TaoCommon::WorkResultCallback);
