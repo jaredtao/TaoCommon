9fdae4d021bafc49f7386944dd59ce74b31144cf
jared <jared2020@163.com>
2020-10-24 12:06:03 +0800

    update format

--- src/TaoCommon/Common/package.h
+++ src/TaoCommon/Common/package.h
@@ -19,12 +19,10 @@ static QByteArrayList unpack(const QByteArray &data)
     quint32 sum = data.size();
     quint32 pos = 0;
     quint32 packageLen = 0;
-    while (pos + headerLength < sum)
-    {
+    while (pos + headerLength < sum) {
         packageLen = 0;
         inStream >> packageLen;
-        if (packageLen <= 0 || packageLen + headerLength > sum - pos)
-        {
+        if (packageLen <= 0 || packageLen + headerLength > sum - pos) {
             break;
         }
         QByteArray subPackage = data.mid(pos + headerLength, packageLen);
